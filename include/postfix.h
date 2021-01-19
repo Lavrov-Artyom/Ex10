a7b894bbbf936852ca08fc4e170d55cf2af5e533
Lavrov-Artyom <artem3sandy@gmail.com>
2021-01-19 13:47:46 +0300

    First

--- include/postfix.h
+++ include/postfix.h
@@ -1,8 +1,14 @@
-// Copyright 2020 A.SHT
+
+
+// Copyright 2020 ArtyomLavrov
+
+
 #ifndef INCLUDE_POSTFIX_H_
 #define INCLUDE_POSTFIX_H_
 #include <string>
 
+
 std::string infix2postfix(std::string infix);
+
 
 #endif  // INCLUDE_POSTFIX_H_
